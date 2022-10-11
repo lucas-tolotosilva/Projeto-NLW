29dc6fd0d29645715f195f69e1dab808ca6cec68
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2022-10-11 15:54:02 -0300

    Adicionando tela de Splash Mobile e Assets

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/index.html',
+    'index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'galaxy': "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #EA72FC 28.08%, #43E7AD 80.94%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    }, 
   },
   plugins: [],
 }
