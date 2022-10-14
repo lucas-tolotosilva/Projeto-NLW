f941450fca347876ffe00bf72638d8e35cb82f89
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2022-10-14 14:01:08 -0300

    Adding Prisma ORM to create the DataBase

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,33 @@
+// Prisma ORM
 import express from 'express';
-
+// Tipos de Parametros
+    /**
+     * Query
+     * Route
+     * Body
+     */
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    //const gameId = request.params.id;
+    
     return response.json([
         {id: 1, name:'Lucas'},
     ])
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+    //const adsId = request.params.id;
+    
+    return response.json([])
 });
 
 app.listen(3030)
